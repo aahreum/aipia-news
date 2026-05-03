082be8aaaad33448b59488f3e33936488fee9eaa
aahreum <cocoding420@gmail.com>
2026-05-03 14:09:35 +0900

    ✨ Feat: SkeletonCard 컴포넌트 구현

---
+++ src/shared/ui/SkeletonCard.tsx
@@ -0,0 +1,12 @@
+export default function SkeletonCard() {
+  return (
+    <div className='flex h-[128px] w-full animate-pulse gap-6 overflow-hidden rounded-xl border border-gray-200 p-6'>
+      <div className='h-full w-[102px] shrink-0 rounded-sm bg-gray-200' />
+      <div className='flex flex-1 flex-col justify-center gap-2'>
+        <div className='h-6 w-4/5 rounded bg-gray-200' />
+        <div className='h-5.5 w-1/3 rounded bg-gray-200' />
+        <div className='h-5.5 w-1/4 rounded bg-gray-200' />
+      </div>
+    </div>
+  );
+}
