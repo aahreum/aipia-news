d95b22b5f33b70434cc88ce5748d6171f1b9d249
aahreum <cocoding420@gmail.com>
2026-05-03 14:55:52 +0900

    🎨 Style: header 스티키 적용 white 컬러 변수 추가

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header className='w-full'>
-      <h1 className='sticky mx-auto w-full max-w-[1200px] pt-6 pb-4 text-2xl font-bold text-primary'>
+    <header className='sticky top-0 z-20 w-full bg-white'>
+      <h1 className='mx-auto w-full max-w-[1200px] pt-6 pb-4 text-2xl font-bold text-primary'>
         <Link href='/'>AIPIA News</Link>
       </h1>
     </header>
