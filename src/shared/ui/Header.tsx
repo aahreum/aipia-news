d4b1f6f7ac782de28f2a48117854e0f0174c8365
aahreum <cocoding420@gmail.com>
2026-05-03 14:49:30 +0900

    ✨ Feat: Header Link 태그 추가

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -1,8 +1,10 @@
+import Link from 'next/link';
+
 export default function Header() {
   return (
     <header className='w-full'>
       <h1 className='sticky mx-auto w-full max-w-[1200px] pt-6 pb-4 text-2xl font-bold text-primary'>
-        AIPIA News
+        <Link href='/'>AIPIA News</Link>
       </h1>
     </header>
   );
