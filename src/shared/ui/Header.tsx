b2c4f0bbd2129a44f892ffd0993239aa309b4480
aahreum <cocoding420@gmail.com>
2026-05-03 14:14:09 +0900

    🎨 Style: header max-width로 변경

--- src/shared/ui/Header.tsx
+++ src/shared/ui/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className='w-full'>
-      <h1 className='sticky mx-auto w-[1200px] pt-6 pb-4 text-2xl font-bold text-primary'>
+      <h1 className='sticky mx-auto w-full max-w-[1200px] pt-6 pb-4 text-2xl font-bold text-primary'>
         AIPIA News
       </h1>
     </header>
