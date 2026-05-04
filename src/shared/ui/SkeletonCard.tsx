02563e8a9b40cbdcf766a040a6902f4e53c4a00e
aahreum <cocoding420@gmail.com>
2026-05-04 14:45:48 +0900

    🎨 Style: 스켈레톤 컴포넌트 스타일 수정

--- src/shared/ui/SkeletonCard.tsx
+++ src/shared/ui/SkeletonCard.tsx
@@ -1,6 +1,6 @@
 export default function SkeletonCard() {
   return (
-    <div className='flex h-[128px] w-full animate-pulse gap-6 overflow-hidden rounded-xl border border-gray-200 p-6'>
+    <div className='flex h-[158px] w-full animate-pulse gap-4 overflow-hidden rounded-2xl border border-gray-200 p-6'>
       <div className='h-full w-[102px] shrink-0 rounded-sm bg-gray-200' />
       <div className='flex flex-1 flex-col justify-center gap-2'>
         <div className='h-6 w-4/5 rounded bg-gray-200' />
