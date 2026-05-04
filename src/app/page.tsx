ab624d0ee6aff304c06d6baf3d5848bc46ec61e5
aahreum <cocoding420@gmail.com>
2026-05-04 15:02:48 +0900

    ✨ Feat: 스켈레톤 컴포넌트 구현 및 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import { Suspense } from 'react';
+import NewsFeedSkeleton from '@/features/news/ui/NewsFeedSkeleton';
 import NewsList from '@/features/news/ui/NewsList';
 import TabBar from '@/features/news/ui/TabBar';
 
 export default function Home() {
   return (
-    // TODO: 스켈레톤 UI 적용하기
-    <Suspense fallback={'불러오는 중'}>
+    <Suspense fallback={<NewsFeedSkeleton />}>
       <TabBar />
       <NewsList />
     </Suspense>
--- src/features/news/ui/SkeletonCard.tsx
+++ src/features/news/ui/SkeletonCard.tsx
@@ -1,11 +1,13 @@
+import SkeletonBox from '@/shared/ui/SkeletonBox';
+
 export default function SkeletonCard() {
   return (
     <div className='flex h-[158px] w-full animate-pulse gap-4 overflow-hidden rounded-2xl border border-gray-200 p-6'>
-      <div className='h-full w-[102px] shrink-0 rounded-sm bg-gray-200' />
+      <SkeletonBox className='h-full w-[102px] shrink-0' />
       <div className='flex flex-1 flex-col justify-center gap-2'>
-        <div className='h-6 w-4/5 rounded bg-gray-200' />
-        <div className='h-5.5 w-1/3 rounded bg-gray-200' />
-        <div className='h-5.5 w-1/4 rounded bg-gray-200' />
+        <SkeletonBox className='h-6 w-4/5' />
+        <SkeletonBox className='h-4 w-1/3' />
+        <SkeletonBox className='h-4 w-1/4' />
       </div>
     </div>
   );
