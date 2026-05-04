fbfdcdb45d5876d7463db0696108c64adf0eb38c
aahreum <cocoding420@gmail.com>
2026-05-04 11:29:49 +0900

    ⚙️ Chore: svgr 설정 추가 및 svg 에셋 추가

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: 'picsum.photos' }],
   },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
 };
 
 export default nextConfig;
