44ffb1489dbc6ead60d42120053c01adea991d2f
aahreum <cocoding420@gmail.com>
2026-05-06 12:55:20 +0900

    ♻️ Refactor: 스켈레톤 스타일 개선

--- src/features/news/ui/NewsFeedSkeleton.tsx
+++ src/features/news/ui/NewsFeedSkeleton.tsx
@@ -1,18 +1,11 @@
-import { PAGE_SIZE } from '@/shared/constants/pageSize';
-import SkeletonCard from './SkeletonCard';
+import SkeletonGrid from './SkeletonGrid';
 import SkeletonTabBar from './SkeletonTabBar';
 
 export default function NewsFeedSkeleton() {
   return (
     <>
       <SkeletonTabBar />
-      <ul className='mt-8 grid gap-6 sm:grid-cols-2 md:grid-cols-3'>
-        {Array.from({ length: PAGE_SIZE }).map((_, i) => (
-          <li key={i}>
-            <SkeletonCard />
-          </li>
-        ))}
-      </ul>
+      <SkeletonGrid />
     </>
   );
 }
--- src/features/news/ui/SkeletonCard.tsx
+++ src/features/news/ui/SkeletonCard.tsx
@@ -3,7 +3,7 @@ import SkeletonBox from '@/shared/ui/SkeletonBox';
 export default function SkeletonCard() {
   return (
     <div className='flex h-[158px] w-full gap-4 overflow-hidden rounded-2xl border border-gray-200 p-6'>
-      <SkeletonBox className='h-full w-[102px] shrink-0' />
+      <SkeletonBox className='h-full w-[102px] shrink-0 rounded-lg' />
       <div className='flex flex-1 flex-col justify-center gap-2'>
         <SkeletonBox className='h-6 w-4/5' />
         <SkeletonBox className='h-4 w-1/3' />
